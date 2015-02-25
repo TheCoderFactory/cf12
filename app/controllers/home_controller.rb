992057b3e720d970189b50fbfefeb7623dec3597
Pete Argent <pete@thecoderfactory.com>
2015-02-25 16:52:52 +1100

    home page changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
-  	@products = Shoppe::Product.active.featured.includes(:product_category, :variants)
+  	@courses = Shoppe::Product.where(product_category: nil)
+  	@upcoming_courses = @courses.where('start_date >= ?', Date.today).limit(5).order(start_date: :asc)
+  	# @products = Shoppe::Product.active.featured.includes(:product_category, :variants)
     # @products = @products.group_by(&:product_category)
   end
 end
