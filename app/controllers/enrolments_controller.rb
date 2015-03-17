72cf2246afd67614a0f106b4e3055761869bdc83
Pete Argent <pete@thecoderfactory.com>
2015-03-18 01:09:52 +1100

    add enrolments page

--- app/controllers/enrolments_controller.rb
+++ app/controllers/enrolments_controller.rb
@@ -2,6 +2,6 @@ class EnrolmentsController < ApplicationController
   def index
   	# @upcoming_courses = Shoppe::Product.active.where('parent_id != ? AND start_date >= ?', nil, DateTime.now).order(name: :asc)
   	@future_courses = Shoppe::Product.active.where('start_date >= ?', Date.today).order(start_date: :asc)
-  	
+  	# Shoppe::Order.ordered.received.includes(:order_items => :ordered_item)
   end
 end
--- app/helpers/enrolments_helper.rb
+++ app/helpers/enrolments_helper.rb
@@ -1,2 +1,9 @@
 module EnrolmentsHelper
+	def enrolment_count(product)
+		Shoppe::Order.ordered.received.includes(:products).where(products: { id: product.id }).count
+	end
 end
+
+# the number of received orders with an order item that is the name of the product
+
+# Shoppe::OrderItem.where(ordered_item_id: future_course.id).count
